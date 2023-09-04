6204bc0bf7b7d1b377ccd56be62d54d3173a529b
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-09-04 16:07:53 -0400

    test: basic information

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,18 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('index page has expected information', async ({ page }) => {
+	// basic information
 	await page.goto('/');
+	await expect(page.title()).resolves.toMatch('svelte-mosaic');
 	await expect(page.getByRole('heading', { name: 'svelte-mosaic' })).toBeVisible();
+
+	// favicon exists
+	const favicon = await page.$('link[rel="icon"]');
+	expect(favicon).toBeTruthy();
+
+	// links
+	const icons = await page.$$('.icons a');
+	expect(icons.length).toBe(2);
+	expect(await icons[0].getAttribute('href')).toBe('https://github.com/LeoDog896/svelte-mosaic');
+	expect(await icons[1].getAttribute('href')).toBe('https://npmjs.com/package/svelte-mosaic');
 });
