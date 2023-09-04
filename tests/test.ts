e1a2a8b9f80726e0eb2c6e507b87d79f9d4690b6
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-09-04 17:42:12 -0400

    feat: dragbar testing

--- tests/test.ts
+++ tests/test.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected information', async ({ page }) => {
 	// basic information
-	await page.goto('/');
+	await page.goto('/svelte-mosaic/');
 	await expect(page.title()).resolves.toMatch('svelte-mosaic');
 	await expect(page.getByRole('heading', { name: 'svelte-mosaic' })).toBeVisible();
 
@@ -15,4 +15,39 @@ test('index page has expected information', async ({ page }) => {
 	expect(icons.length).toBe(2);
 	expect(await icons[0].getAttribute('href')).toBe('https://github.com/LeoDog896/svelte-mosaic');
 	expect(await icons[1].getAttribute('href')).toBe('https://npmjs.com/package/svelte-mosaic');
+});
+
+test('dragbar can be dragged', async ({ page }) => {
+	await page.goto('/svelte-mosaic/');
+
+	// dragbar exists
+	const dragbar = await page.$('.sash.sash-horizontal');
+	expect(dragbar).toBeTruthy();
+
+	if (!dragbar) {
+		return;
+	}
+
+	// dragbar can be dragged
+	const dragbarBox = await dragbar.boundingBox();
+	expect(dragbarBox).toBeTruthy();
+	if (!dragbarBox) {
+		return;
+	}
+
+	const oldDragBarX = dragbarBox.x;
+
+	await page.mouse.move(dragbarBox.x + dragbarBox.width / 2, dragbarBox.y + dragbarBox.height / 2);
+	await page.mouse.down();
+	await page.mouse.move(dragbarBox.x + dragbarBox.width / 2 + 100, dragbarBox.y + dragbarBox.height / 2);
+	await page.mouse.up();
+
+	// check if the bar has moved
+	const newDragBarX = (await dragbar.boundingBox())?.x;
+	expect(newDragBarX).toBeTruthy();
+	if (!newDragBarX) {
+		return;
+	}
+	 
+	expect(newDragBarX).toBeGreaterThan(oldDragBarX);
 });
